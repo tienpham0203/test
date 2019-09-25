3658df70c8da42523070aed1359a5fe6c48c6966
Tien <tien.pham@sjsu.edu>
2019-09-24 23:44:15 -0700

    new code added at 11:44

--- hello.py
+++ hello.py
@@ -1,5 +1,17 @@
-def main ():
-    print("this is Tien, this is the second test, newest one")
 
-if __name__ == '__main__':
-    main()
+        def main():
+            print("this is Tien, this is the second test, newest one")
+
+        if __name__ == '__main__':
+            main()
+
+
+            def myFun(x):
+                x[0] = 20
+
+
+            # Driver Code (Note that lst is modified
+            # after function call.
+            lst = [10, 11, 12, 13, 14, 15]
+            myFun(lst);
+            print(lst)
